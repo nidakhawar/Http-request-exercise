4aae7db5db1df908afcf6c310debfaad0d3d13d5
nidakhawar <nidakhawar@yahoo.com>
2020-04-08 15:56:28 +0000

    animal noise generator

---
+++ requestsTask/application1/application/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from application import app
+import requests
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/get/animal', methods = ['GET', 'POST'])
+def animal():
+    animal = requests.get('http://application2:5001/animal/name')
+    noise = requests.post('http://application2:5001/animal/noise', data=animal.text)
+    return render_template('animals.html', title='Animals', animal=animal.text, noise=noise.text)
+    
+
---
+++ requestsTask/application2/application/routes.py
@@ -0,0 +1,21 @@
+from application import app
+from flask import request, Response
+from random import randint
+
+@app.route('/animal/name', methods = ['GET'])
+def animal_name():
+    animals = ['cat', 'dog', 'cow']
+    return Response(animals[randint(0,2)], mimetype='text/plain')
+
+@app.route('/animal/noise', methods = ['POST'])
+def animal_noise():
+    animal = request.data.decode("utf-8")
+    if animal == "cat":
+        noise = "meow"
+    elif animal == "dog":
+        noise = "woof"
+    elif animal == "cow":
+        noise = "moo"
+    else:
+        noise = "We din't know the noise of this animal"
+    return Response(noise, mimetype='text/plain')
